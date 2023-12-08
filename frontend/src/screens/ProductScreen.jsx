b6fc2d533f767064440b440ef13aad1dd9ec13e0
summereth <liq.qian.l@gmail.com>
2023-12-07 20:47:30 -0800

    Finish product detail page

---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, Card, ListGroup, ListGroupItem, Button } from 'react-bootstrap'
+import products from '../products'
+import Rating from '../components/Rating'
+
+const ProductScreen = () => {
+    const {id: productID} = useParams();
+    const product = products.find((p) => p._id === productID);
+
+    return (
+        <>
+            <Link className='btn btn-light my-3' to='/'>
+                Go Back
+            </Link>
+            <Row>
+                <Col md={5}>
+                    {/* fluid makes image flexible */}
+                    <Image src={product.image} alt={product.name} fluid/>
+                </Col>
+                <Col md={4}>
+                    <ListGroup variant='flush'>
+                        <ListGroupItem>
+                            <h3>{product.name}</h3>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            Description: ${product.description}
+                        </ListGroupItem>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>${product.price}</Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>Stock:</Col>
+                                    <Col>{product.countInStock > 9 ? "In stock" : product.countInStock > 0 ? `Only ${product.countInStock} left` : "Out of stock"}</Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
+                                    Add To Cart
+                                </Button>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default ProductScreen
